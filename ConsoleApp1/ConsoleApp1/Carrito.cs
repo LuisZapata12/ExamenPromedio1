507c0ffd252e41b49e45d41c746ed260ae778ba6
LuisZapata12 <luis.98.30.16@gmail.com>
2023-09-28 21:15:08 -0500

    Producto,ProductoArcilla,Tela y carrito scripts

---
+++ ConsoleApp1/ConsoleApp1/Carrito.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Carrito
+    {
+
+        private List<Producto> productosEnCarrito;
+
+        public Carrito()
+        {
+            productosEnCarrito = new List<Producto>();
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            productosEnCarrito.Add(producto);
+        }
+
+        public int PrecioTotal()
+        {
+            int precioTotal = 0;
+            foreach (var producto in productosEnCarrito)
+            {
+                precioTotal += producto.Precio;
+            }
+            return precioTotal;
+        }
+
+        public void MostrarContenido()
+        {
+            Console.WriteLine("Contenido del Carrito de Compras:");
+            foreach (var producto in productosEnCarrito)
+            {
+                Console.WriteLine(producto.ObtenerCaracteristicas());
+            }
+            Console.WriteLine("Precio Total: " + PrecioTotal());
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    interface Producto
+    {
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+
+        public virtual string ObtenerCaracteristicas()
+        {
+            return "Nombre: " + Nombre + ", Precio: " + Precio;
+        }
+
+    }
+}
